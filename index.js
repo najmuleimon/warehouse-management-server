020031baf0176dbfb68a24dfbc9eac2fe27d69a1
najmuleimon <najmul.eimon@gmail.com>
2022-04-29 14:23:18 +0600

    db connection added

--- index.js
+++ index.js
@@ -7,8 +7,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5tt4v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+console.log('db connected');
+  // perform actions on the collection object
+  client.close();
+});
+
+
 app.get('/', (req, res) => {
-    res.send('Running wirehouse server');
+    res.send('Running warehouse server');
 })
 app.listen(port, () => {
     console.log('Listening to port', port);
