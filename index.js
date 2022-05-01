50c01814534820f84e38ad1b1112514f3bf981cd
najmuleimon <najmul.eimon@gmail.com>
2022-05-01 10:49:27 +0600

    products added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -8,15 +9,32 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5tt4v.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-console.log('db connected');
-  // perform actions on the collection object
-  client.close();
-});
+
+
+async function run(){
+  try{
+    await client.connect();
+    const productCollection = client.db('proTech').collection('product');
+
+    // get or read all products
+    app.get('/products', async(req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    
+    console.log("DB connected");
+  });
+
+  }
+  finally{
+
+  }
+}
+run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
